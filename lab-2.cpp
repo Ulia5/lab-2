f3785491a5fdba6f9685a425c8742df3b6c7d324
Yulia <YG.YG.flower@gmail.com>
2021-12-28 11:01:19 +0700

    C++_changes(lab_11)

--- lab-2.cpp
+++ lab-2.cpp
@@ -26,12 +26,12 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	// Объявление и инициализация объектов, используемых в примерах
-	Theatre* theatres = new Theatre[3];
+	Theatre* theatres = new Theatre[2];
 	Stage* stages[2]; 
 	Performance* performances[2];
 	Performance* performances0[1];
 	Actor actors[3];
-	StageDirector stageDirectors[2];
+	StageDirector stageDirectors[2][1];
 	for (int i = 0; i < 2; i++) {
 		stages[i] = new Stage;
 	}
@@ -42,18 +42,10 @@ int main()
 		performances0[i] = new Performance;
 	}
 	// Заполнение информации об объектах, используемых в примерах
-	int age;
-	try { 
-		cin >> age; 
-		ageEx(age);
-	}
-	catch (exception& ex) {
-		cout << ex.what() << endl;
-	};
-	actors[0].setActor((char*)"Иван", (char*)"Иванов", age, &theatres[0]);
-	actors[1].setActor((char*)"Петр", (char*)"Петров", age, &theatres[0]);
-	stageDirectors[0].setStageDirector((char*)"Игорь", (char*)"Иванов", 23, &theatres[0]);
-	stageDirectors[1].setStageDirector((char*)"Павел", (char*)"Петров", 30, &theatres[0]);
+	actors[0].setActor((char*)"Иван", (char*)"Иванов", 32, &theatres[0]);
+	actors[1].setActor((char*)"Петр", (char*)"Петров", 35, &theatres[0]);
+	stageDirectors[0][0].setStageDirector((char*)"Игорь", (char*)"Иванов", 23, &theatres[0]);
+	stageDirectors[1][0].setStageDirector((char*)"Павел", (char*)"Петров", 30, &theatres[0]);
 	performances[0]->setPerformance("Игра", 200, 2020);
 	performances[1]->setPerformance("Мир", 220, 2000);
 	performances0[0]->setPerformance("Лира", 120, 2021);
@@ -62,6 +54,16 @@ int main()
 	stages[0]->setStage((char*)"Большая сцена", 500, 2, performances);
 	stages[1]->setStage((char*)"Малая сцена", 100, 1, performances0);
 	theatres[0].setTheatre((char*)"Музыкальный театр", 1990, 2, stages);
+
+	// Примеер для задания №1
+	cout << "Задание №1\n" << endl;
+	cout << "Одномерный массив:" << endl;
+	for (int i = 0; i < 2; i++)
+		performances[i]->toString();
+	cout << "Двумерный массив:" << endl;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 1; j++)
+			stageDirectors[i][j].toString();
 	// Освобождение памяти
 	delete[] theatres;
 	for (int i = 0; i < 2; i++) {
